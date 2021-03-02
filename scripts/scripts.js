2577e2c0802b2f299a0510d5ec96bd4fd9df6e6b
Danil <danil.snip@gmail.com>
2021-03-02 16:15:22 +0500

    js-4

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 
 
-function old(){
+ function old(){
     let Name = prompt("Введите имя");
     let yearOfBirth = +prompt("Введите ваш год рождения");
     let year = 2021;
@@ -9,13 +9,55 @@ function old(){
     let str2 = (" Ваш возраст ");
     let yearOld = year - yearOfBirth;
     if (!isNaN(Name) || isNaN(yearOfBirth)){
-        alert("Данные введины не правильно!");
+        alert("Данные введены не правильно!");
     }else{
-        alert("Данные введены правильно!");
+        return str1 + Name + str2 + yearOld;
     }
-    return str1 + Name + str2 + yearOld;
 }
-alert(old());
+alert(old()); 
 
 
 //////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////
+
+
+ function randomer(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+
+
+var col = +prompt("Введите количество решаемых примеров");
+var max = 20;
+
+
+for( i = 0; i < col; i++ ){
+    var num1 = randomer(max);
+    var num2 = randomer(max);
+    var randNum = Math.floor(Math.random() * 2 + 1);
+
+    if(randNum == 1){
+        result = num1 + num2;
+        var primer = +prompt("Дайте ваш ответ " + num1 + "+" + num2 + "=");
+    } else if (randNum == 2){
+        result = num1 - num2;
+        var primer = +prompt("Дайте ваш ответ " + num1 + "-" + num2 + "=");
+    }else if (randNum == 3){
+        result = num1 * num2;
+        var primer = +prompt("Дайте ваш ответ " + num1 + "*" + num2 + "=");
+    }else if (randNum == 4){
+        result = num1 / num2;
+        var primer = +prompt("Дайте ваш ответ " + num1 + ":" + num2 + "=");
+    }
+
+
+    if (result == primer){
+        alert("Правильно " + result);
+    }else{
+        alert(" Не правильно " + " Ваш ответ " + primer + " Правильный ответ " + result );
+    }
+} 
+
+
+////////////////////////////////////////////////////////
